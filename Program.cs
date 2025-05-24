d0385634fe8c3b843626601316d75d76a0979632
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-24 17:42:09 +0700

    Commit
    Add Content

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhập số lượng số nguyên tố: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int count = 0;
+        int n = 2;
+
+        while (count < number)
+        {
+
+            bool soNguyenTo = true;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    soNguyenTo = false;
+                    break;
+                }
+            }
+            if (soNguyenTo)
+            {
+                Console.WriteLine(n);
+                count += 1;
+            }
+            n += 1;
+        }
+        
+
+        
+    }
+}
